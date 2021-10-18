1db4a2d4abce919f51d8203b4295f7bc6ae46569
Wanyi Zhang <wz866@nyu.edu>
2021-10-18 12:07:06 -0400

    finished delete supplier and list all suppliers and tests

--- app.py
+++ app.py
@@ -77,6 +77,42 @@ def create_supplier() -> Tuple[Response, int]:
         return error_response(str(e), 400)
     return jsonify(id=created_supplier.id), 200
 
+@app.route("/supplier/<int:supplier_id>", methods=["DELETE"])
+def delete_supplier(supplier_id) -> Tuple[Response, int]:
+    """Deletes a supplier"""
+    app.logger.info("Deletes a supplier with id: {}".format(supplier_id))
+    """Here I use the find() function implemented by Iris directly."""
+    supplier = database.find(supplier_id)
+    if not supplier:
+        missing_msg = "Supplier with id: {} was not found".format(supplier_id)
+        app.logger.info(missing_msg)
+        return error_response(missing_msg, 400)
+    supplier.delete()
+    deleted_msg = "Supplier with id: {} was deleted".format(supplier_id)
+    app.logger.info(deleted_msg)
+    return Response(deleted_msg), 200
+
+@app.route("/suppliers", methods=["GET"])
+def list_all_suppliers() -> Tuple[Response, int]:
+    """List all suppliers"""
+    app.logger.info("List all suppliers")
+    suppliers = database.get_suppliers()
+    if suppliers:
+        for id, supplier in suppliers:
+            supplier_name = supplier.name
+            supplier_email = supplier.email
+            supplier_address = supplier.address
+            supplier_products = supplier.products
+
+            app.logger.info("Supplier information:" + "\nid: " + str(id) + 
+            "\nname: " + supplier_name + "\nemail: " + supplier_email +
+            "\naddress: " + supplier_address)
+        return Response("All suppliers are listed above"), 200        
+    else:
+        empty_msg = "There are no suppliers"
+        app.logger.info(empty_msg)
+        return Response(empty_msg), 200
+
 ######################################################################
 #   Convenience functions
 ######################################################################
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from app import app
 import json
+import unittest
 
 test_app = app.test_client()
 
@@ -36,3 +37,57 @@ def test_create_incorrect_data_type():
     assert response.status_code == 400
     assert response.is_json
     assert 'error' in response.json
+
+def test_delete_non_empty_database():
+    test_app.put('/supplier',
+                            data=json.dumps({
+                                'name': 'test1',
+                                'email': 'nice2'
+                            }), content_type='application/json')
+
+    test_app.put('/supplier',
+                            data=json.dumps({
+                                'name': 'test2',
+                                'email': 'nice2'
+                            }), content_type='application/json')
+
+    response1 = test_app.delete("/supplier/1")
+    assert response1 is not None
+    assert response1.status_code == 200
+    
+    response2 = test_app.detete("/supplier/1")
+    assert response2 is not None
+    assert response2.status_code == 200
+
+def test_delete_empty_database():
+    response = test_app.delete("/supplier/1")
+    assert response is not None
+    assert response.status_code == 400
+    assert response.is_json
+    assert 'error' in response.json
+
+def test_list_all_non_empty_database():
+    test_app.put('/supplier',
+                            data=json.dumps({
+                                'name': 'test1',
+                                'email': 'nice2'
+                            }), content_type='application/json')
+
+    test_app.put('/supplier',
+                            data=json.dumps({
+                                'name': 'test2',
+                                'email': 'nice2'
+                            }), content_type='application/json')
+
+    response = test_app.get("/suppliers)")
+    assert response is not None
+    assert response.status_code == 200
+    
+def test_list_all_empty_database():
+    response = test_app.get("/suppliers)")
+    assert response is not None
+    assert response.status_code == 200
+
+    
+
+
